489c4dabbec960b2bba952083befa035b92b11dc
Anh Quach <anh.quachquituan@halliburton.com>
2021-08-07 03:33:15 +0700

    Update README.md and description of the outputs

--- output.tf
+++ output.tf
@@ -1,9 +1,11 @@
 output "value" {
-    value = azurerm_key_vault_secret.credential.value
-    sensitive   = true
+  value = azurerm_key_vault_secret.credential.value
+  sensitive   = true
+  description = "Private SSH Key or Random Password"
 }
 
 output "ssh_public_key" {
   value = local.public_ssh_key
   sensitive = true
+  description = "Public SSH Key that can be used as input to an VM"
 }
